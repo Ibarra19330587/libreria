c6f68b2718aa66cae078d458a35584fe0743c76a
Ibarra19330587 <72420795+Ibarra19330587@users.noreply.github.com>
2020-10-19 18:03:44 -0700

    Add files via upload

---
+++ Prueba_Libreria/Prueba_Libreria/Operaciones.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba_Libreria
+{
+    public class Operaciones
+    { 
+        public static double Suma (double num1, double num2)
+        {
+            return num1 + num2 ;
+        }
+
+        public static double Resta (double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public static double Multiplicacion (double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public static double Division(double num1, double num2)
+        {
+                return num1 / num2;
+        }
+    }
+}
